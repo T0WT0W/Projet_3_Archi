780a32e44cb3a6e383ca47d72afa28cdb1271d8f
Thomas Pierre <tomlo4@hotmail.com>
2024-04-22 18:34:54 +0200

    “Desktop”

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -2,21 +2,15 @@ axios.get('http://localhost:5678/api/works')
   .then(function (response) {
     const works = response.data
     let gallery = document.getElementById("gallery")
-    let nouvelleFigure = document.createElement('figure')
-    let image = document.createElement('img')
-    image.src = "assets/images/hotel-first-arte-new-delhi.png"
-    let figcaption = document.createElement('figcaption')
-    figcaption.textContent = "Hotel First Arte - New Delhi"
-
-    gallery.append(nouvelleFigure)
-    nouvelleFigure.append(image)
-    nouvelleFigure.append(figcaption)
-
+ 
     for (let work of works) {
-        console.log(work.title)
+        let nouvelleFigure = document.createElement('figure')
+        let image = document.createElement('img')
+        image.src = work.imageUrl
+        let figcaption = document.createElement('figcaption')
+        figcaption.textContent = work.title
+        gallery.append(nouvelleFigure)
+        nouvelleFigure.append(image)
+        nouvelleFigure.append(figcaption)
     }
-
-
-
-
   })
