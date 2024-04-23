e1acab9529af9c92f299953d836b29cd0b359363
Thomas Pierre <tomlo4@hotmail.com>
2024-04-23 19:53:11 +0200

    “maj”

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,7 +1,18 @@
 axios.get('http://localhost:5678/api/works')
   .then(function (response) {
     const works = response.data
+    categoryFilters(null)
+
+  axios.get('http://localhost:5678/api/categories')
+  .then(function (response) {
+    const categories = response.data
+  })
+
+  function categoryFilters (event) {
+    if(event)
+      console.log(event.target.dataset.id)
     let gallery = document.getElementById("gallery")
+    gallery.innerHTML = ""
  
     for (let work of works) {
         let nouvelleFigure = document.createElement('figure')
@@ -12,5 +23,12 @@ axios.get('http://localhost:5678/api/works')
         gallery.append(nouvelleFigure)
         nouvelleFigure.append(image)
         nouvelleFigure.append(figcaption)
-    }
-  })
+        }
+      }
+
+  let aFilters = document.querySelectorAll("a.filters");
+
+  Array.from(aFilters).forEach(btn=>
+      btn.addEventListener("click", categoryFilters)
+  )
+})
