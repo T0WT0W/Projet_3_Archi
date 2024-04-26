f0c91cf90448f632cecb73267c27b8bf5fa8edd0
Thomas Pierre <tomlo4@hotmail.com>
2024-04-26 15:28:00 +0200

    “maj”

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,20 +1,30 @@
+// appelle les works de l'API
 axios.get('http://localhost:5678/api/works')
   .then(function (response) {
-    const works = response.data
+    let works = response.data
     categoryFilters(null)
 
+  // appelle les catégories de l'API
   axios.get('http://localhost:5678/api/categories')
   .then(function (response) {
     const categories = response.data
+    // rajouter la création des éléments HTML des catégories (for(let category of categories)...)
   })
 
+  // Fonction qui permet d'afficher les works selon une catégorie (ou pas)
   function categoryFilters (event) {
-    if(event)
-      console.log(event.target.dataset.id)
+    // récupère la galerie et supprime tous ses éléments
     let gallery = document.getElementById("gallery")
     gallery.innerHTML = ""
- 
-    for (let work of works) {
+    
+    // récupère le numéro de la catégorie si l'event en trouve un 
+    const categoryId = event ? event.target.getAttribute("data-id"): null
+    // si j'ai une catégorie, alors je filtre mes works
+    const memoriesWorks = categoryId ? works.filter(w => w.categoryId == categoryId): works
+
+    // boucle les works, qu'ils soient filtrés ou pas
+    for (let work of memoriesWorks) {
+        // créer les éléments HTML
         let nouvelleFigure = document.createElement('figure')
         let image = document.createElement('img')
         image.src = work.imageUrl
@@ -26,9 +36,12 @@ axios.get('http://localhost:5678/api/works')
         }
       }
 
-  let aFilters = document.querySelectorAll("a.filters");
+  // récupère tous les éléments "button.filters" du HTML
+  let buttonFilters = document.querySelectorAll("button.filters");
 
-  Array.from(aFilters).forEach(btn=>
-      btn.addEventListener("click", categoryFilters)
-  )
+  // boucle sur tous les button récupérés
+  Array.from(buttonFilters).forEach((btn)=> {
+    // attribue l'événement "écoute le clic" à chacun de mes button, pour chaque clic exécute la fonction "catégoryFilters"
+    btn.addEventListener("click", (event)=> categoryFilters(event))
+  })
 })
